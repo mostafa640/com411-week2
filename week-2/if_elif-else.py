870df0e726647e1ab0969e6b068c1eda686ec180
mostafa640 <91554099+mostafa640@users.noreply.github.com>
2021-10-07 02:04:31 +0100

    if elif statment

---
+++ week-2/if_elif-else.py
@@ -0,0 +1,15 @@
+print("Towards which direction should I paint (up, down, left or right)?")
+direction = input()
+if direction == "up":
+    print("I am painting in the upward direction!")
+elif direction == "down":
+    print("I am painting in the downward direction!")
+elif direction == "left":
+    print("I am painting in the leftward direction!")
+
+
+elif direction == "right":
+    print("I am painting in the rightward direction!")
+
+else:
+    print("not sure which direction means")
