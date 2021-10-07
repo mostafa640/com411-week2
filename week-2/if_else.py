e26fd7094f0001a5bea3ab6c19d09b429d66763b
mostafa640 <91554099+mostafa640@users.noreply.github.com>
2021-10-07 01:36:28 +0100

    activity

---
+++ week-2/if_else.py
@@ -0,0 +1,7 @@
+print("Please enter the activity to be performed")
+activity = input()
+if activity == "calculate":
+    print("Performing calculations...")
+
+else:
+    print("Activity completed!")
