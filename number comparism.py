1f03c5298868997048cae2e05b5d5ef82a801d1c
Mostafa Kamal <5kamam28@solent.ac.uk>
2021-10-07 11:46:13 +0100

    counter solve

--- number comparism.py
+++ number comparism.py
@@ -1,7 +1,9 @@
 print("please enter the first number. ")
 first_number = int(input())
+
 print("please enter the second number. ")
 second_number = int(input())
+
 if first_number < second_number:
     print("first number is the smallest. ")
 elif first_number > second_number:
